c045e2644bcfd63b4488c8edffd1491def22749d
dangthaison <dangthaison8354@gmail.com>
2023-11-07 17:37:48 +0700

    commit header code list

---
+++ src/assets/imgs/index.js
@@ -0,0 +1,3 @@
+export { default as IconVN } from "./vietnam 1.png";
+export { default as IconUSA } from "./united-states 1.png";
+export { default as Icon2Chiu } from "./Frame 4.png";
---
+++ src/components/common/(main)/(article)/ArticleList/index.jsx
@@ -0,0 +1,9 @@
+function ArticleList() {
+  return (
+    <>
+      <h1>Article List</h1>
+    </>
+  );
+}
+
+export default ArticleList;
